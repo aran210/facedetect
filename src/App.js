95d914f0acb9cc51274fccb5a1e1e180ae660542
Aran <aran.ubhi@me.com>
2020-07-21 19:42:58 +0100

    Move API calls to server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'; 
-import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -12,9 +11,6 @@ import Register from './components/Register/Register'
 
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: 'd8f8eb2f27a543fc82a2dc5b7c217203'
- });
 
 const particleOptions = {
   particles: {
@@ -93,7 +89,14 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageURL: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+    })
+    .then(response => response.json())
     .then(response => { 
       if (response) {
         fetch('http://localhost:3000/image', {
